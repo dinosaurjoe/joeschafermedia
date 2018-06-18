2db53d73d79c82fcfa05ac84f01a5987aae4b6e4
Joe Schafer <joeschaferastula@gmail.com>
2018-06-18 15:10:37 -0500

    Initial categories added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(name: 'Ruby')
+Category.create(name: 'Javascript')
+Category.create(name: 'React JS')
+Category.create(name: 'Angular JS')
+Category.create(name: 'Go')
+Category.create(name: 'Digital Media')
