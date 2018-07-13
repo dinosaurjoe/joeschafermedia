cb76fed8e7b64fe0f9c8aed0ac30bbc1b151722d
Joe Schafer <joeschaferastula@gmail.com>
2018-07-13 09:06:35 -0500

    Card converted to react component

--- app/javascript/components/Post.js
+++ app/javascript/components/Post.js
@@ -5,8 +5,13 @@ class Post extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div className="card-description">
-          <a href={this.props.path}>{this.props.title}</a>
+        <div className="card">
+          <div className="card-category">
+            <h3>{this.props.category}</h3>
+          </div>
+          <div className="card-description">
+            <a href={this.props.path}>{this.props.title}</a>
+          </div>
         </div>
       </React.Fragment>
     );
@@ -15,7 +20,8 @@ class Post extends React.Component {
 
 Post.propTypes = {
   title: PropTypes.string,
-  path: PropTypes.string
+  path: PropTypes.string,
+  category: PropTypes.string
 };
 
 export default Post
